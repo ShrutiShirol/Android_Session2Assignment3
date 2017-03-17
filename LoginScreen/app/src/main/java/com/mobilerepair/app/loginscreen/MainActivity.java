728fa31753cdcff8e214b00c42eba35a0b396d44
shrutishirol <shrutishirol3@gmail.com>
2017-03-17 23:29:55 +0530

    initial commit

---
+++ LoginScreen/app/src/main/java/com/mobilerepair/app/loginscreen/MainActivity.java
@@ -0,0 +1,60 @@
+package com.mobilerepair.app.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt_email,edt_password;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt_email = (EditText)findViewById(R.id.edttxt_email);
+        edt_password = (EditText)findViewById(R.id.edttxt_password);
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String email = edt_email.getText().toString();
+                if (!isValidEmail(email)) {
+                    edt_email.setError("Invalid Email");
+                }
+
+                final String pass = edt_password.getText().toString();
+                if (!isValidPassword(pass)) {
+                    edt_password.setError("Invalid Password");
+                }
+            }
+        });
+
+    }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password with retype password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() > 6) {
+            return true;
+        }
+        return false;
+    }
+}
